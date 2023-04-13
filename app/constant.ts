cab79bd9d2074f30804f4df8dea999c160a67b0b
heyibear <heyibear@gmail.com>
2023-04-13 21:30:05 +0800

    ChatGPT Bot Web

---
+++ app/constant.ts
@@ -0,0 +1,8 @@
+export const OWNER = "heyibear";
+export const REPO = "ChatGPT-Bot-Web";
+export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
+export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
